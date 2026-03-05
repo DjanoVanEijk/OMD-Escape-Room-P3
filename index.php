07e8e712e228958f758d0fcb1c4e3df1404a2b80
DjanoVanEijk <9028315@student.zadkine.nl>
2026-03-05 16:11:01 +0100

    homepage

--- index.php
+++ index.php
@@ -5,20 +5,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Escape Room</title>
-  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="css/style.css">
+  
 </head>
-
+<header></header>
 <body>
-
+  <img src="img/footagecrate-4k-rectangular-stamp-guilty@2X.webp" alt="">
   <h1>Welkom</h1>
   <p>De button hieronder is alleen voor testdoeleinden bij het opstarten van het project. Deze pagina is bedoeld voor de
     uitleg van jullie escape room.</p>
   <p>Via deze pagina gaat een team naar de aanmeldpagina</p>
 
-
-  <button><a href="./rooms/room_1.php">Klik hier voor een demonstratie van kamer
-      1</a></button>
-
+  <button><a href="./rooms/room_1.php">Start</a></button>
+  <button><a href="./rooms/room_1.php">Reviews</a></button>
+  
 </body>
 
 </html>
