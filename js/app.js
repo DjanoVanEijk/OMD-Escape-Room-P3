fe12342124e31021e57958e6af494e2ae1f9305c
MustafaTavasli10 <9014651@student.zadkine.nl>
2026-03-06 11:39:47 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -56,3 +56,34 @@ function checkAnswer() {
     feedback.style.color = 'red';
   }
 }
+
+// Timer functionaliteit
+
+let tijd = 1500;
+
+let timer;
+
+function formatTijd(seconden) {
+  let min = Math.floor(seconden / 60);
+  let sec = seconden % 60;
+  if (sec < 10) sec = "0" + sec;
+  return min + ":" + sec;
+}
+
+function update() {
+  let display = document.getElementById("timers");
+  display.innerHTML = formatTijd(tijd);
+}
+
+function end() {
+  tijd--;
+  update();
+  
+  if (tijd === 0) {
+    clearInterval(timer);
+    alert("Klaar!");
+    document.querySelector("body").style.backgroundColor = "green"
+  }
+}
+
+timer = setInterval(end, 1000);
