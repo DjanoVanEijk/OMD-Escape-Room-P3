c6be4dbbf6093c4150d585f3cb6234558aaa3078
MustafaTavasli10 <9014651@student.zadkine.nl>
2026-03-17 13:46:47 +0100

    Update index, css en javascript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+// Deze functie opent de modal voor teamregistratie
+
+function openModal_1() {
+  document.getElementById('overlay1').style.display = 'block';
+  document.getElementById('modal1').style.display = 'block';
+
+}
+
 // Deze functie opent de modal en toont de vraag
 function openModal(index) {
   // Zoek het element met de class 'box' en het bijbehorende data-index
