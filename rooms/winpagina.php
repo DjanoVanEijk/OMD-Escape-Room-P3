b492e45156662af76ad6db335396369b8cbd258f
DjanoVanEijk <9028315@student.zadkine.nl>
2026-04-10 12:44:02 +0200

    winpagina

--- rooms/winpagina.php
+++ rooms/winpagina.php
@@ -22,31 +22,29 @@
     <script src="../js/app.js" defer></script>
 </head>
 <body class="winpagina">
-    <h1>Je hebt gewonnen!!</h1>
-    <div>  
-        <div class="windiv">
-            <img class="winimg" src="../img/winimg.webp" alt="afbeelding">
-            <div class="tekstdeel">
-                <h2 class="winp">Na een gevaarlijke reis <br>
-                bent je er eindelijk in geslaagd te ontsnappen.</h2>
-                <form action="overzichtPagina.php" method="post" class="formreview">
-                    <h2 class="review">Review</h2>
-                    <input type="number" name="rating" id="rating" placeholder="star rating" max=5 min=0>
-                    <textarea type="text" name="description" id="description" placeholder="description"></textarea>
-                    <input type="submit" value="Submit" id="reviewsub">
-                    <?php 
-                        $rating = $_POST['rating'] ?? '';
-                        if (isset($_POST['rating'])) {
-                            echo "Vul het eerst in!";    
-                        };
-                        $description = $_POST['description'] ?? '';
-                        if (isset($_POST['description'])) {
-                            echo "Vul het eerst in!";
-                        }
-                    ?>
-                </form>
-            </div>
-        </div>
-    </div>    
+    <h1>Je hebt gewonnen!</h1>
+    <div class="winflex">
+    <div class="wincontainer">
+    <img class="winimg" src="../img/winimg.png" alt="afbeelding">
+    <h2 class="winp">Na een gevaarlijke reis bent je er eindelijk in geslaagd te ontsnappen.</h2>
+    </div>
+
+    <form action="overzichtPagina.php" method="post" class="formreview">
+    <h2 class="review">Review</h2>
+    <input type="number" name="rating" id="rating" placeholder="star rating" max=5 min=0>
+    <textarea type="text" name="description" id="description" placeholder="description"></textarea>
+    <input type="submit" value="Submit" id="reviewsub">
+    <?php 
+    $rating = $_POST['rating'] ?? '';
+    if (isset($_POST['rating'])) {
+    echo "Vul het eerst in!";    
+    };
+    $description = $_POST['description'] ?? '';
+    if (isset($_POST['description'])) {
+    echo "Vul het eerst in!";
+    }
+    ?>
+    </form>
+    </div>
 </body>
 </html>
