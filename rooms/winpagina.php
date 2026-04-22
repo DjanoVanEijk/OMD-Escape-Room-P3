01ca7f54976ac2a217f43142f659ed7c4f0b5003
DjanoVanEijk <9028315@student.zadkine.nl>
2026-04-22 11:00:42 +0200

    Revert "fix"
    This reverts commit e9d4c5bc969cb87f9cad03fb785977c6ba2c42d5.

--- rooms/winpagina.php
+++ rooms/winpagina.php
@@ -8,6 +8,8 @@ try {
     die("Databasefout: " . $e->getMessage());
 };
 
+//$sql = "INSERT INTO overzicht (rating, review)
+    //VALUES (rating, descriptions)"; 
 
 ?>
 <!DOCTYPE html>
@@ -30,7 +32,22 @@ try {
             <input type="number" name="rating" value="rating" id="rating" placeholder="star rating" max="5" min="0">
             <textarea name="descriptions" value="descriptions" id="descriptions" placeholder="Review met feedback en moeilijkheid"></textarea>
             <input type="submit" value="Submit" id="reviewsub">
-        </form>
+            <?php 
+                $rating = $_POST['rating'] ?? '';
+                if (isset($_POST['rating'])) {
+                    echo "Vul het eerst in!";    
+                };
+                $descriptions = $_POST['descriptions'] ?? '';
+                if (isset($_POST['descriptions'])) {
+                    echo "Vul het eerst in!";    
+                };
+                $stmt = $db_connection->prepare("INSERT INTO overzicht (rating, review) VALUES (:rating, :descriptons)");
+                $stmt->execute([
+                    ':rating' => $rating,
+                    ':review' => $review
+                ]);
+            ?>
+        </form>3
     </div>
 </body>
 </html>
