0e12eda5675c279c4fcb9ba0fc38a5a42d97573e
MustafaTavasli10 <9014651@student.zadkine.nl>
2026-04-22 10:11:27 +0200

    update room_1,2 en 3

--- rooms/room_1.php
+++ rooms/room_1.php
@@ -2,17 +2,11 @@
 session_start(); // Start de sessie om bij $_SESSION te kunnen
 require_once('../dbcon.php');
 
-// Controleer of er een teamnaam is, anders terug naar begin
-if (!isset($_SESSION['teamname'])) {
-    header("Location: ../index.php");
-    exit();
-}
-
 try {
-    $stmt = $db_connection->query("SELECT * FROM riddles WHERE roomId = 1");
-    $riddles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $stmt = $db_connection->query("SELECT * FROM riddles WHERE roomId = 2");
+  $riddles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    die("Databasefout: " . $e->getMessage());
+  die("Databasefout: " . $e->getMessage());
 }
 ?>
 
@@ -28,7 +22,11 @@ try {
   <div class="flex2">
     <h1 class="info" id="timers">00:00</h1> 
     <h1 class=info>
-      Team: <?php echo htmlspecialchars($_SESSION['teamname'] ?? 'Geen team'); ?>
+      <?php $teamnaam = $_POST['teamname'] ?? '';
+        if (isset($_POST['teamname'])) {
+        echo "Team: $teamnaam";
+        };
+      ?>
     </h1>  
     <div class="hints-drop">
       <h1 class="info">?</h1>
@@ -42,6 +40,24 @@ try {
         </ul>
       </div>
     </div>
+  </div>
+  <div class="playersName">
+    <h2>
+      <?php
+        if (isset($_POST['name1']))
+        {
+          echo "Speler 1: " . $_POST['name1'];
+        }
+        if (isset($_POST['name2']))
+        {
+          echo " | Speler 2: " . $_POST['name2'];
+        }
+        if (isset($_POST['name3']))
+        {
+          echo " | Speler 3: " . $_POST['name3'];
+        }
+      ?>
+    </h2>
   </div>
   <div id="overlayverlies">
     <div class="verliesflex">
